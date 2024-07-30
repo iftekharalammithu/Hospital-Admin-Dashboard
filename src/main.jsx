be47b8e46e216c7398b5326fa20cdcf57111eb05
IftekharAlamMithu <iftekharalammithu23@gmail.com>
2024-07-30 10:09:25 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import React, { createContext, useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+const AuthContext = createContext({ IsAuth: false });
+
+const Appwrapper = () => {
+  const [IsAuth, setAuth] = useState(false);
+  const [user, setuser] = useState({});
+  return (
+    <AuthContext.Provider value={{ IsAuth, setAuth, user, setuser }}>
+      <App />
+    </AuthContext.Provider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Appwrapper />
+  </React.StrictMode>
+);
